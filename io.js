4a7ff82acf1f682f8dd216e4a5fc8506f67c170e
ynunokawa <yusuke_nunokawa@esrij.com>
2015-06-25 10:53:56 +0900

    test

--- io.js
+++ io.js
@@ -14,12 +14,16 @@ function io(server) {
       });
     });*/
 
+    var store = {};
+
     io.on('connection', function (socket) {
       socket.on('join', function(msg) {
         console.log('MESSAGE: ', msg);
-        socket.set('room', msg.roomid);
-        socket.set('name', msg.name);
-        socket.set('id', msg.id);
+        usrobj = {
+          'room': msg.roomid,
+          'name': msg.name
+        };
+        store[msg.id] = usrobj;
         socket.join(msg.roomid);
         roomid = msg.roomid;
         //io.emit('join', msg);
@@ -28,38 +32,22 @@ function io(server) {
       socket.on('chat message', function(msg) {
         var roomid;
         console.log('MESSAGE: ', msg);
-        socket.get('room', function(err, _room) {
-          roomid = _room;
-        });
-        io.to(roomid).emit('chat message', msg);
+        io.to(store[msg.id].roomid).emit('chat message', msg);
       });
       socket.on('map message', function(msg) {
         var roomid;
         console.log('MESSAGE: ', msg);
-        socket.get('room', function(err, _room) {
-          roomid = _room;
-        });
-        io.to(roomid).emit('map message', msg);
+        io.to(store[msg.id].roomid).emit('map message', msg);
       });
 
-      socket.on('disconnect', function() {
-        var roomid, name, id;
-        socket.get('room', function(err, _room) {
-          roomid = _room;
-        });
-        socket.get('name', function(err, _name) {
-          name = _name;
-        });
-        socket.get('id', function(err, _id) {
-          id = _id;
-        });
+      /*socket.on('disconnect', function() {
         socket.leave(roomid);
         io.to(roomid).emit('chat message', {
           id: id,
           name: name,
           text: '退出しました！'
         });
-      })
+      });*/
     });
 }
 
